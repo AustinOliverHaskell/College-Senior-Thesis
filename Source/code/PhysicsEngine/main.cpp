980b8ac9297d583d5586b66ce0bad0d3e0a1bb1e
Austin Oliver Haskell <ahaskell@carthage.edu>
2017-11-10 18:14:34 -0600

    Added File Manip Classes

--- Source/code/PhysicsEngine/main.cpp
+++ Source/code/PhysicsEngine/main.cpp
@@ -1,74 +1,27 @@
-#include <iostream>
+#include "BasicExample.h"
 
-#include <btBulletDynamicsCommon.h>
+#include "CommonExampleInterface.h"
+#include "CommonGUIHelperInterface.h"
+#include "BulletCollision/CollisionDispatch/btCollisionObject.h"
+#include "BulletCollision/CollisionShapes/btCollisionShape.h"
+#include "BulletDynamics/Dynamics/btDiscreteDynamicsWorld.h"
 
-int main (void)
+#include "LinearMath/btTransform.h"
+#include "LinearMath/btHashMap.h"
+
+int main(int argc, char* argv[])
 {
-		std::cout << " - Thesis - Austin Haskell - " << std::endl;
+	
+	DummyGUIHelper noGfx;
 
+	CommonExampleOptions options(&noGfx);
+	CommonExampleInterface*    example = BasicExampleCreateFunc(options);
+	
+	example->initPhysics();
+	example->stepSimulation(1.f/60.f);
+	example->exitPhysics();
 
-        btBroadphaseInterface* broadphase = new btDbvtBroadphase();
+	delete example;
 
-        btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
-        btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
-
-        btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
-
-        btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
-
-        dynamicsWorld->setGravity(btVector3(0, -10, 0));
-
-
-        btCollisionShape* groundShape = new btStaticPlaneShape(btVector3(0, 1, 0), 1);
-
-        btCollisionShape* fallShape = new btSphereShape(1);
-
-
-        btDefaultMotionState* groundMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, -1, 0)));
-        btRigidBody::btRigidBodyConstructionInfo
-                groundRigidBodyCI(0, groundMotionState, groundShape, btVector3(0, 0, 0));
-        btRigidBody* groundRigidBody = new btRigidBody(groundRigidBodyCI);
-        dynamicsWorld->addRigidBody(groundRigidBody);
-
-
-        btDefaultMotionState* fallMotionState =
-                new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 50, 0)));
-        btScalar mass = 1;
-        btVector3 fallInertia(0, 0, 0);
-        fallShape->calculateLocalInertia(mass, fallInertia);
-        btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI(mass, fallMotionState, fallShape, fallInertia);
-        btRigidBody* fallRigidBody = new btRigidBody(fallRigidBodyCI);
-        dynamicsWorld->addRigidBody(fallRigidBody);
-
-
-        for (int i = 0; i < 300; i++) {
-                dynamicsWorld->stepSimulation(1 / 60.f, 10);
-
-                btTransform trans;
-                fallRigidBody->getMotionState()->getWorldTransform(trans);
-
-                std::cout << "sphere height: " << trans.getOrigin().getY() << std::endl;
-        }
-
-        dynamicsWorld->removeRigidBody(fallRigidBody);
-        delete fallRigidBody->getMotionState();
-        delete fallRigidBody;
-
-        dynamicsWorld->removeRigidBody(groundRigidBody);
-        delete groundRigidBody->getMotionState();
-        delete groundRigidBody;
-
-
-        delete fallShape;
-
-        delete groundShape;
-
-
-        delete dynamicsWorld;
-        delete solver;
-        delete collisionConfiguration;
-        delete dispatcher;
-        delete broadphase;
-
-        return 0;
+	return 0;
 }
---
+++ Source/code/StructureFile.java
@@ -0,0 +1,14 @@
+package austin.structures;
+
+import java.util.*;
+import java.io.*;
+
+/**
+*	This class is my own personal file format that holds the .obj
+*	 file representation of a structure as well as some other information
+*	 about the building in question
+*/
+public class StructureFile
+{
+	
+}
